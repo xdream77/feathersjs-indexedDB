dd8c3593fd60f9ca417d02cc8702abc0f9a61026
xdream77 <andre.kotthaus@gmail.com>
2021-11-07 10:04:35 +0100

    add single service

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,6 @@ import feathers from '@feathersjs/feathers';
 import createService from './service.js';
 const local = feathers();
 
-const makeService = name => local.use(name, createService({ name }));
+export const makeLocalService = name => (local.use(name, createService({ name })), local);
 
-export const makeLocalServices = names => (names.map(makeService), local);
+export const makeLocalServices = names => (names.map(makeLocalService), local);
